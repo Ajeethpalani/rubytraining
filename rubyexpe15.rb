fc082ba1f2398b625475f1354e05a33828a16512
Ajeethpalani <93540006+Ajeethpalani@users.noreply.github.com>
2021-11-08 22:59:48 +0530

    Add files via upload

---
+++ rubyexpe15.rb
@@ -0,0 +1,15 @@
+filename = "D:\ruby-ajeeth ruby_ajeeth.txt" 
+
+prompt = "> "
+txt = File.open(filename)
+
+puts "Here's your file: #{filename} \n"
+puts txt.read()
+
+puts "I'll also ask you to type it again: "
+print prompt
+file_again = STDIN.gets.chomp()
+
+txt_again = File.open(file_again)
+
+puts txt_again.read()
